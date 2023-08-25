21a6f3634bb7f109a1c25e3f44595eee6de1cd01
cassidoxa <cassidymoen@gmx.com>
2023-08-25 01:56:01 -0400

    Fix Link Face Update ROM

--- Rom.py
+++ Rom.py
@@ -19,7 +19,7 @@ from EntranceShuffle import door_addresses
 
 
 JAP10HASH = '03a63945398191337e896e5771f77173'
-RANDOMIZERBASEHASH = 'b25a9618a3109bbc516a8043e89ff80e'
+RANDOMIZERBASEHASH = 'ef317b669a76dd25cb6642004dd41b22'
 
 
 class JsonRom(object):
