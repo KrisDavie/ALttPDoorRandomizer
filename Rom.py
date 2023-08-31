55896cd3d9af54052885433f97aba8537dcb981d
cassidoxa <cassidymoen@gmx.com>
2023-08-31 15:22:20 -0400

    Update ROM

--- Rom.py
+++ Rom.py
@@ -19,7 +19,7 @@ from EntranceShuffle import door_addresses
 
 
 JAP10HASH = '03a63945398191337e896e5771f77173'
-RANDOMIZERBASEHASH = '89992e317eda0d724d310c1fdc585d69'
+RANDOMIZERBASEHASH = 'ca221541366c9bff294880e5cf751b69'
 
 
 class JsonRom(object):
